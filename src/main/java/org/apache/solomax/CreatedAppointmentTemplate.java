e2d53c39c93902dd514f63dad974554244c527a3
Maxim Solodovnik <solomax666@gmail.com>
2016-10-14 10:23:07 +0700

    Initial commit

---
+++ src/main/java/org/apache/solomax/CreatedAppointmentTemplate.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License") +  you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.solomax;
+
+import java.util.Date;
+
+import org.apache.wicket.core.util.string.ComponentRenderer;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.panel.Fragment;
+
+public class CreatedAppointmentTemplate extends AbstractAppointmentTemplate {
+	private static final long serialVersionUID = 1L;
+
+	public CreatedAppointmentTemplate() {
+		super();
+
+		add(new Label("titleLbl", "Title Label"));
+		add(new Label("title", "Title"));
+		add(new WebMarkupContainer("descContainer")
+			.add(new Label("descLbl", "Description Label"))
+			.add(new Label("desc", "Description"))
+			);
+	}
+
+	@Override
+	public String getSubject() {
+		Fragment f = new Fragment(COMP_ID, "subject", this);
+		f.add(new Label("prefix", "Prefix").setRenderBodyOnly(true)
+				, new Label("title", "Title").setRenderBodyOnly(true)
+				, new Label("start", new Date().toString()).setRenderBodyOnly(true)
+				, new Label("end", new Date().toString()).setRenderBodyOnly(true)
+				);
+		return ComponentRenderer.renderComponent(f).toString();
+	}
+}
---
+++ src/main/java/org/apache/solomax/HomePage.java
@@ -0,0 +1,20 @@
+package org.apache.solomax;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.WebPage;
+
+public class HomePage extends WebPage {
+	private static final long serialVersionUID = 1L;
+
+	public HomePage(final PageParameters parameters) {
+		super(parameters);
+
+		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+
+		CreatedAppointmentTemplate temp = new CreatedAppointmentTemplate();
+		//add(new Label("subject", temp.getSubject()).setEscapeModelStrings(false));
+		add(new Label("subject", "Subject"));
+		add(new Label("body", temp.getEmail()).setEscapeModelStrings(false));
+	}
+}
